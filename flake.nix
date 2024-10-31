53630e5610be42b56158e5438456ef14165fd8af
soriphoono <soriphoono@gmail.com>
2024-10-31 16:01:24 -0500

    Deleted python backend, too complex

--- flake.nix
+++ flake.nix
@@ -9,35 +9,17 @@
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { pkgs, ... }: let 
-        pythonEnv = pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
-          django
-          jinja2
-        ]);
-      in {
+      perSystem = { pkgs, ... }: {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             nixd
             nixpkgs-fmt
 
-            pythonEnv
+            nodejs
+          
+            yarn
+            yarn2nix
           ];
-        };
- 
-        apps = let 
-          debug = pkgs.writeShellApplication {
-            name = "debug.sh";
-
-            runtimeInputs = [
-              pythonEnv
-            ];
-
-            text = ''
-              
-            '';
-          };
-        in  {
-          debug.program = "${debug}/bin/debug.sh";
         };
       };
     };
